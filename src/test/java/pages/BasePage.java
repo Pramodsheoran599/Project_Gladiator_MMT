2e8570c2f3032a0ff79b91dd8ed5c18e96b99a52
Rutvik Panchal <rutvik.rp2901@gmail.com>
2021-08-12 16:30:29 +0530

    - Updated BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,6 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
@@ -8,5 +11,10 @@ public class BasePage {
 	
 	protected BasePage(WebDriver driver) {
 		this.driver = driver;
+	}
+	
+	protected void waitTillVisibilityOfElement(By element) {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(element));
 	}
 }
