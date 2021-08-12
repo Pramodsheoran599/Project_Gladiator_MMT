0650b8f683dab8f683710cf7cf6d4b96b63170c5
Rutvik Panchal <rutvik.rp2901@gmail.com>
2021-08-12 16:36:01 +0530

    - Updated BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,5 +17,10 @@ public class BasePage {
 	protected void waitTillVisibilityOfElement(By element) {
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+	}
+	
+	protected void scrollPageVertically(int pixels) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0," + pixels + ")");
 	}
 }
