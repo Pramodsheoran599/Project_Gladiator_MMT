c4e106ae0932b2a19410c772f5b07b3e01ba75f6
abhinashmalakar1947 <abhinashmalakar0017@gmail.com>
2021-08-13 15:37:44 +0530

    4th Commit

--- src/test/java/Tests/RoundTripFlight.java
+++ src/test/java/Tests/RoundTripFlight.java
@@ -17,21 +17,23 @@ public class RoundTripFlight extends BaseTest {
 		driver.findElement(By.className("userLoggedOut")).click();
 		driver.findElement(By.cssSelector("li[data-cy='roundTrip']")).click();
 		
-		 driver.findElement(By.cssSelector("button[type='button']")).click();
+		// driver.findElement(By.cssSelector("button[type='button']")).click();
 		
 		driver.findElement(By.linkText("SEARCH")).click();
 		//driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div/div[3]/button")).click();
-		
+		/*
 		 Set wnd = driver.getWindowHandles();
 	      // window handles iterate
 	      Iterator i = wnd.iterator();
 	      String popwnd = (String) i.next();
 	      String prntw = (String) i.next();
-	      driver.findElement(By.linkText("OKAY, GOT IT!")).click();
+	      
 	      // switching pop up tab
-	      driver.switchTo().window(popwnd);
+	      driver.switchTo().window(prntw);
+	      driver.findElement(By.linkText("OKAY, GOT IT!")).click();
+		*/
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div/div[3]/button")).click();
 		
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[2]/span")).click();
 		driver.findElement(By.linkText("Book Now")).click();
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[2]/button")).click();
 	Thread.sleep(5000);
