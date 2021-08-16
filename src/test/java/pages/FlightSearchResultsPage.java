5904b9ff64d853535f4f75ff5ac2f21963c1be61
Rutvik Panchal <rutvik.rp2901@gmail.com>
2021-08-16 09:10:04 +0530

    - Added some test cases

--- src/test/java/pages/FlightSearchResultsPage.java
+++ src/test/java/pages/FlightSearchResultsPage.java
@@ -1,8 +1,15 @@
 package pages;
 
 import frameworks.BasePage;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class FlightSearchResultsPage extends BasePage {
 
@@ -11,17 +18,88 @@ public class FlightSearchResultsPage extends BasePage {
 	}
 
 	By flights = By.cssSelector(".listingCard");
-	By multiCityFlights = By.cssSelector(".listingCard ");
+	By multiCityFlights = By.cssSelector("button[id^='bookbutton']");
+
+	By viewPricesButton = By.xpath("//button[@id='bookbutton-RKEY:c71da437-423a-47c5-ad07-54d1222a7e49:16_0']//span[@class='appendRight8'][normalize-space()='View Prices']");
+	By bookNowButton = By.cssSelector("button.fli_primary_btn");
 
 	public int getCountOfFlights() {
 		return driver.findElements(flights).size();
 	}
 
 	public int getCountOfFlights(boolean multiCity) {
-//		waitTillVisibilityOfElement(flights);
 		if (multiCity)
 			return driver.findElements(multiCityFlights).size();
 		else
 			return driver.findElements(flights).size();
 	}
+	
+	protected void jsClick(WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+	
+	public void bookFirstFlight() {
+		
+		try {
+			waitTillVisibilityOfElement(By.xpath("//span[@class='bgProperties icon20 overlayCrossIcon']"));
+			driver.findElement(By.xpath("//span[@class='bgProperties icon20 overlayCrossIcon']")).click();
+		} catch (NoSuchElementException e) {
+
+		} catch (TimeoutException e) {
+
+		}
+
+		List<WebElement> ele = driver.findElements(By.cssSelector("[id^=bookbutton]"));
+		System.out.println(ele.size());
+		ele.get(0).click();
+		
+		scrollPageVertically(150);
+		
+		List<WebElement> bookNowButtons = driver.findElements(By.cssSelector("[id^=bookbutton]"));
+		System.out.println(bookNowButtons.size());
+		for (WebElement elem: bookNowButtons) {
+			System.out.println(elem.getText());
+			if (elem.getText().equalsIgnoreCase("book now")) {
+				elem.click();
+				System.out.println("Book Now clicked");
+				break;
+			}
+		}
+	}
+	
+	public void bookFirstMultiCItyFlight() {
+		try {
+			waitTillVisibilityOfElement(By.xpath("//span[@class='bgProperties icon20 overlayCrossIcon']"));
+			driver.findElement(By.xpath("//span[@class='bgProperties icon20 overlayCrossIcon']")).click();
+		} catch (NoSuchElementException e) {
+			
+		} catch(TimeoutException e) {
+			
+		}
+		
+		List<WebElement> ele = driver.findElements(By.cssSelector("[class*='ViewFareBtn']"));
+		System.out.println(ele.size());
+		ele.get(0).click();
+		
+		scrollPageVertically(150);
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<WebElement> bookNowButtons = driver.findElements(By.cssSelector("button[class*='btn']"));
+		System.out.println(bookNowButtons.size());
+		for (WebElement elem: bookNowButtons) {
+			System.out.println(elem.getText());
+			if (elem.getText().equalsIgnoreCase("book now")) {
+				elem.click();
+				System.out.println("Book Now clicked");
+				break;
+			}
+		}
+	}
+	
 }
--- src/test/java/tests/FlightOneWayTripTests.java
+++ src/test/java/tests/FlightOneWayTripTests.java
@@ -1,5 +1,9 @@
 package tests;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,28 +13,136 @@ import pages.Search;
 
 public class FlightOneWayTripTests extends BaseTest {
 
-    Search search;
-    FlightSearchResultsPage flightSearchResults;
+	Search search;
+	FlightSearchResultsPage flightSearchResults;
+	boolean isReviewDetail = false;
 
-    @Test
-    public void tc_flight_search02() throws InterruptedException
-    {
-        driver.get(object_repository.getProperty("homepage_url"));
-        search = new Search(driver);
+	@Test(description = "To Test if flights are available")
+	public void tc_flight_search02() throws InterruptedException {
+		driver.get(object_repository.getProperty("homepage_url"));
 
-        search.selectFromCity();
-        search.searchFromCity("Mumbai");
-        search.searchToCity("Mumbai");
+		search = new Search(driver);
+		search.selectFromCity();
+		search.searchFromCity("Mumbai");
+		search.searchToCity("Mumbai");
 
-        Assert.assertEquals(true, search.isSameCityErrorVisible());
-    }
+		Assert.assertEquals(true, search.isSameCityErrorVisible());
+	}
 
-    @Test(dependsOnMethods = "tc_flight_search02")
-    public void tc_flight_search01() {
+	@Test(dependsOnMethods = "tc_flight_search02", description = "To test flights with same arrival and departure  destination")
+	public void tc_flight_search01() {
+		String fromCity = "Mumbai";
+		String toCity = "Delhi";
+		String date = "20-08-2021";
 
-        search.searchOneWayTripFlights("Mumbai", "Delhi", "15-08-2021");
-        flightSearchResults = new FlightSearchResultsPage(driver);
+		search.searchOneWayTripFlights(fromCity, toCity, date);
+		flightSearchResults = new FlightSearchResultsPage(driver);
+		Assert.assertTrue(flightSearchResults.getCountOfFlights() > 0);
+	}
 
-        Assert.assertTrue(flightSearchResults.getCountOfFlights() > 0);
-    }
+	@Test(dependsOnMethods = "tc_flight_search01", description = "To test Book now feature")
+	public void tc_flight_select01() {
+		flightSearchResults.bookFirstFlight();
+		flightSearchResults.switchToNewTab();
+
+		Assert.assertTrue(driver.getCurrentUrl().contains("review"));
+	}
+
+	@Test(dependsOnMethods = "tc_flight_select01", description = "To test review page")
+	public void tc_flight_review02() throws Exception {
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		if (url.contains("reviewDetails")) {
+			isReviewDetail = true;
+
+			List<WebElement> but = driver.findElements(
+					By.cssSelector("button[class='lato-black button buttonPrimary extraPadBtn fontSize16 ']"));
+			for (WebElement b : but) {
+				if (b.getText().equalsIgnoreCase("continue")) {
+					b.click();
+				}
+			}
+			Assert.assertTrue(
+					driver.findElements(By.cssSelector("span[class='errorMsg fontSize14 appendLeft5']")).size() != 0);
+		} else {
+			flightSearchResults.executeMouseClick(driver.findElement(By.cssSelector("button[id='review-continue']")));
+
+			Assert.assertTrue(driver.findElements(By.cssSelector("p[class='validation-error digit-validation-error']"))
+					.size() != 0);
+		}
+	}
+
+	@Test(dependsOnMethods = "tc_flight_review02", description = "To test review page")
+	public void tc_flight_review01() {
+		if (isReviewDetail) {
+			flightSearchResults.executeMouseClick(
+					driver.findElement(By.cssSelector("span[class='darkText lightFont fontSize14 appendLeft10']")));
+			Assert.assertTrue(driver.getCurrentUrl().contains("review"));
+		} else {
+
+		}
+	}
+
+	@Test(dependsOnMethods = "tc_flight_review01", description = "To test continue button without filling mandatory details")
+	public void tc_flight_traveller01() {
+		if (isReviewDetail) {
+			flightSearchResults.executeMouseClick(
+					driver.findElement(By.cssSelector("span[class='darkText lightFont fontSize14 appendLeft10']")));
+
+			flightSearchResults.waitTillVisibilityOfElement(By.cssSelector("button[class='addTravellerBtn']"));
+			flightSearchResults
+					.executeMouseClick(driver.findElement(By.cssSelector("button[class='addTravellerBtn']")));
+			
+			driver.findElement(By.cssSelector("input[placeholder='First & Middle Name']")).sendKeys("Rutvik");
+			driver.findElement(By.cssSelector("input[placeholder='Last Name']")).sendKeys("Panchal");
+			driver.findElement(By.cssSelector("label[tabindex='0']")).click();
+			driver.findElement(By.cssSelector("input[placeholder='Mobile No']")).sendKeys("1234567890");
+			driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("test@test.com");
+			
+			flightSearchResults.executeMouseClick(
+					driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.extraPadBtn.fontSize16 ")));
+			flightSearchResults.executeMouseClick(
+					driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.extraPadBtn.fontSize16 ")));
+
+			flightSearchResults.switchToNewTab();
+			
+		} else {
+
+		}
+	}
+
+//	@Test(dependsOnMethods = "tc_flight_search01")
+//	public void tc_flight_search05() {
+//		Actions act = new Actions(driver);
+//
+//		driver.findElement(By.cssSelector("span[class='darkText lightFont fontSize14 appendLeft10']")).click();
+//
+//		flightSearchResults.executeMouseClick(driver.findElement(By.cssSelector("button[class='addTravellerBtn']")));
+//
+//		driver.findElement(By.cssSelector("input[placeholder='First & Middle Name']")).sendKeys("Rutvik");
+//		driver.findElement(By.cssSelector("input[placeholder='Last Name']")).sendKeys("Panchal");
+//		driver.findElement(By.cssSelector("label[tabindex='0']")).click();
+//		driver.findElement(By.cssSelector("input[placeholder='Mobile No']")).sendKeys("1234567890");
+//		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("test@test.com");
+//
+//		flightSearchResults.executeMouseClick(
+//				driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.extraPadBtn.fontSize16 ")));
+//		flightSearchResults.executeMouseClick(
+//				driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.extraPadBtn.fontSize16 ")));
+//
+//		flightSearchResults.switchToNewTab();
+//
+//		act.moveToElement(driver.findElement(By.cssSelector(".button.buttonPrimary.buttonBig.fontSize14"))).click()
+//				.perform();
+//
+//		act.moveToElement(driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.buttonBig.fontSize12")))
+//				.click().perform();
+//		act.moveToElement(
+//				driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.extraPadBtn.fontSize16 "))).click()
+//				.perform();
+//		act.moveToElement(
+//				driver.findElement(By.cssSelector(".lato-black.button.buttonPrimary.extraPadBtn.fontSize16 "))).click()
+//				.perform();
+//
+//	}
 }
